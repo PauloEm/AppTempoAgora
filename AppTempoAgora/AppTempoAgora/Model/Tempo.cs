6842173763d6dc43290bc3c790d5d034a63ea0d5
Usuario <Usuario@CPD03-M10>
2022-09-27 12:13:17 -0300

    Adicionar arquivos de projeto.

---
+++ AppTempoAgora/AppTempoAgora/Model/Tempo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AppTempoAgora.Model
+{
+    public class Tempo : ContentPage
+    {
+        public Tempo()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
